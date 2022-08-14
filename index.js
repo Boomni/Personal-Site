de2058cacc6375e2d6deae1de2f6ab466742875c
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-08-14 22:25:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 favouriteMovieGenre("romantic")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("tomato")
+favouriteFruit("watermelon")
 
 // light, dark
 favouriteMode("light")
